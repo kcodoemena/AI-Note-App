7cc8e558c35574d659bc731b5793fb661224c1bb
Kelechi Odoemena <55855644+kcodoemena@users.noreply.github.com>
2025-05-03 10:09:22 +0100

    Delete app/sentiment.py

--- app/sentiment.py
+++
@@ -1,9 +0,0 @@
-from textblob import TextBlob
-
-def analyze_sentiment(text: str) -> str:
-    polarity = TextBlob(text).sentiment.polarity
-    if polarity > 0.1:
-        return "positive"
-    elif polarity < -0.1:
-        return "negative"
-    return "neutral"
