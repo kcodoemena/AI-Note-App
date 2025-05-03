195a6eb2b553c35925bbd4901a0596bdda64aa19
Kelechi Odoemena <55855644+kcodoemena@users.noreply.github.com>
2025-05-03 10:09:59 +0100

    Delete app/main.py

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from . import models, database
-from .routers import notes, auth
-
-app = FastAPI()
-models.Base.metadata.create_all(bind=database.engine)
-app.include_router(auth.router)
-app.include_router(notes.router)
